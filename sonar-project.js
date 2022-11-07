ebd37bbf539cf9795bf411c93eb90f05a7dcbbb9
fabrizzio aranda <fabrizzio1996@gmail.com>
2022-11-06 22:27:30 -0500

    added sonar token

--- sonar-project.js
+++ sonar-project.js
@@ -1,6 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
      sonarqubeScanner({
        serverUrl: 'http://192.168.18.128:9000',
+       token: `${process.env.SONAR_TOKEN}`,
        options : {
        'sonar.sources': '.',
        //'sonar.inclusions' : '.' // Entry point of your code
